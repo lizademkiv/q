b1a9a531430732c53e9f93caa6dcb294af8145cd
lizademkiv <lizademkiv2@gmail.com>
2024-08-04 15:18:11 +0300

    Add files via upload

---
+++ import sys.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel,QMainWindow
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Hello PyQt")
+        self.setGeometry(100,100,600,400)
+        self.label=QLabel("Hello, PyQt!",self)
+        self.label.setGeometry(50,50,300,50)
+app=QApplication(sys.argv)
+window=MainWindow()
+window.show()
+sys.exit(app.exec_())
